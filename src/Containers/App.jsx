8266da3a02cbd28be24839b2eecc8d0bc3397ce1
Jaime Dordio <jaimedordio@gmail.com>
2020-03-22 02:55:51 +0100

    First commit

---
+++ src/Containers/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import cloneDeep from "clone-deep";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+
+export default App;
